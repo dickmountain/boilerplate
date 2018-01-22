b44d3863b797c5550595057e3d378894716551ee
maxatroshenko <maxatrr@gmail.com>
2018-01-22 17:08:45 +0300

    Change Password added

--- resources/views/account/layouts/partials/_navigation.blade.php
+++ resources/views/account/layouts/partials/_navigation.blade.php
@@ -1,4 +1,5 @@
 <ul class="nav nav-pills nav-stacked">
     <li><a href="{{ route('account.index') }}">Account overview</a></li>
     <li><a href="{{ route('account.profile.index') }}">Profile</a></li>
+    <li><a href="{{ route('account.password.index') }}">Change password</a></li>
 </ul>
---
+++ resources/views/emails/account/password/updated.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Password Updated
+
+Your password was updated
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
