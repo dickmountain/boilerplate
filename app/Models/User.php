e99e098fae3667181c97a4b64a86d65a8c768e7c
maxatroshenko <maxatrr@gmail.com>
2018-01-29 15:23:16 +0300

    Preventing sign in before activation

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,4 +28,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function hasActivated()
+    {
+    	return $this->activated;
+    }
+
+	public function hasNotActivated()
+	{
+		return !$this->hasActivated();
+	}
 }
